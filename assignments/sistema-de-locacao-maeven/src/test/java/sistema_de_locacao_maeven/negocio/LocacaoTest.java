64ad80f2a8c4b6eaaf5856e5ab32274fbf4e9c9a
izaiasmachado <izaiasmachado.dev@gmail.com>
2022-09-06 13:55:02 -0300

    test: killed all mutants

--- assignments/sistema-de-locacao-maeven/src/test/java/sistema_de_locacao_maeven/negocio/LocacaoTest.java
+++ assignments/sistema-de-locacao-maeven/src/test/java/sistema_de_locacao_maeven/negocio/LocacaoTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.After;
 import org.junit.Before;
@@ -17,8 +19,8 @@ public class LocacaoTest {
 	Filme filme;
 	Locacao locacao;
 	double valor;
+	DateFormat formatoData = new SimpleDateFormat("yyyy-MM-dd");  
 	
-	@SuppressWarnings("deprecation")
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
 	
@@ -36,10 +38,17 @@ public class LocacaoTest {
 
 	@Test
 	public void testAlugarClienteFilme() throws Exception {
+		Date dataAtual = new Date();
+		String dataAtualString = formatoData.format(dataAtual);
+
 		locacao.alugar(cliente, filme);			
 		assertEquals(locacao.getFilme(), filme);
 		assertEquals(locacao.getCliente(), cliente);
 		assertEquals(locacao.getValor(), 0, 0.01);
+		assertEquals(dataAtualString, locacao.getDataLocacao());
+		
+		System.out.println(locacao.getHoraLocacao());
+		assertTrue(isValidTime(locacao.getHoraLocacao()));
 	}
 	
 	@Test
@@ -55,10 +64,15 @@ public class LocacaoTest {
 
 	@Test
 	public void testAlugarClienteFilmeDouble() throws Exception {
+		Date dataAtual = new Date();
+		String dataAtualString = formatoData.format(dataAtual);
+
 		locacao.alugar(cliente, filme, valor);			
 		assertEquals(locacao.getFilme(), filme);
 		assertEquals(locacao.getCliente(), cliente);
 		assertEquals(locacao.getValor(), valor, 0.01);
+		assertEquals(dataAtualString, locacao.getDataLocacao());
+		assertTrue(isValidTime(locacao.getHoraLocacao()));
 	}
 	
 	@Test
@@ -75,7 +89,6 @@ public class LocacaoTest {
 	@Test
 	public void testGetDesconto() throws Exception {
 		Filme filme2 = new Filme(1, "JavaLand", GeneroEnum.ROMANCE, 1000.00);
-		Cliente cliente2 = new Cliente(2, "Pedro");
 		Locacao locacao2 = new Locacao();
 		
 		GeneroFactory factory = GeneroFactory.getInstance();
@@ -83,22 +96,23 @@ public class LocacaoTest {
 
 		double desconto = 0.15;
 		romance.setDesconto(desconto);
-		
-		locacao2.alugar(cliente2, filme2);
-		
+				
 		double valorComDesconto = locacao2.getDesconto(valor, filme2);
 		double porcentagemDesconto = 1 - (valorComDesconto / valor);
 		assertEquals(desconto, porcentagemDesconto, 0.01);
 	}
 	
-	@Test
-	public void testGetDataLocacao() throws Exception {
-		DateFormat formatoData = new SimpleDateFormat("yyyy-MM-dd");  
-
-		Date dataAtual = new Date();
-		String dataAtualString = formatoData.format(dataAtual);
-		
-		locacao.alugar(cliente, filme);
-		assertEquals(dataAtualString, locacao.getDataLocacao());
-	}
+	
+	   public static boolean isValidTime(String time) {
+	        String regex = "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
+	 
+	        Pattern p = Pattern.compile(regex);
+	 
+	        if (time == null) {
+	            return false;
+	        }
+	 
+	        Matcher m = p.matcher(time);
+	        return m.matches();
+	    }
 }
